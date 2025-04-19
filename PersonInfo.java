ed5feacceadf4de0faa6fda4b91cd6df86b15111
SpinachFilms <142277271+SpinachFilms@users.noreply.github.com>
2025-04-18 21:09:35 -0500

    Code and pseudocode added

---
+++ PersonInfo.java
@@ -0,0 +1,17 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        // Información ficticia
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "123 Elm Street";
+        String city = "Springfield";
+        String zipCode = "12345";
+
+        // Imprimir en líneas separadas
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CSC320-1-Module-1
 Critical Thinking Assignment
+
+Pseudocode
+
+Home
+
+Define First Name as "John"
+Define Last Name as "Doe"
+Define Address as "123 Elm Street"
+Define City as "Springfield"
+Define Zip Code as "12345"
+
+Print First Name
+Print Last Name
+Print Address
+Print City
+Print Zip Code
+
+Final
