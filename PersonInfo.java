575fadc24488b7e02a6065961efa7b0097cfb2a3
SpinachFilms <142277271+SpinachFilms@users.noreply.github.com>
2025-04-18 21:13:42 -0500

    Bug fixed

--- PersonInfo.java
+++ PersonInfo.java
@@ -1,13 +1,13 @@
 public class PersonInfo {
     public static void main(String[] args) {
-        // Información ficticia
+        
         String firstName = "John";
         String lastName = "Doe";
         String streetAddress = "123 Elm Street";
         String city = "Springfield";
         String zipCode = "12345";
 
-        // Imprimir en líneas separadas
+        
         System.out.println(firstName);
         System.out.println(lastName);
         System.out.println(streetAddress);
